44d06d153d1302f6106db1902844da14b87f4d3b
JoseAntonio1DAM <usr4034@salesianos-lacuesta.net>
2025-10-15 10:42:13 +0100

    hemos hecho la parte supeior de bateria y hora, que no hacia falta, pero no me acorde git add .

--- App.tsx
+++ App.tsx
@@ -1,10 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { MaterialIcons, FontAwesome, Ionicons } from "@expo/vector-icons";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.BarraSuperior}>
+        <Text style={styles.BarraTexto}>18:46</Text>
+        <Text style={styles.BarraTexto2}>97% </Text>
+        <MaterialIcons
+          name="wifi"
+          size={20}
+          color="#fff"
+          style={{ left: 280 }}
+        />
+        <MaterialIcons
+          name="signal-cellular-4-bar"
+          size={20}
+          color="white"
+          style={{ left: 280 }}
+        />
+        <MaterialIcons
+          name="battery-full"
+          size={20}
+          color="white"
+          style={{ left: 320 }}
+        />
+      </View>
+      <Text>YOUTUBE</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#3cac20ff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  // Text: {
+  //   color: "green",
+  // },
+
+  BarraSuperior: {
+    /// marginBottom: 180,
+    width: "100%",
+    borderRadius: 5,
+    position: "absolute",
+    top: 40,
+    height: 40,
+    paddingTop: 10,
+    flexDirection: "row",
+  },
+  BarraTexto: {
+    color: "white",
+    left: 10,
+  },
+  BarraTexto2: {
+    color: "white",
+    position: "absolute",
+    right: 15,
+    top: 10,
   },
 });
